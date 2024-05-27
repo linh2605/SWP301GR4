b2be2503cf96751b9b0a7cf0794623ed4696d1a9
LinhVQ <linhvqhe176018@fpt.edu.vn>
2024-05-28 04:38:06 +0700

    fix nullpoiterexception

--- SWP301_GR4_SE1842/src/java/dal/CategoryDAO.java
+++ SWP301_GR4_SE1842/src/java/dal/CategoryDAO.java
@@ -11,7 +11,7 @@ public class CategoryDAO extends DBContext {
     // Read a category by its ID
     public Category getCategoryById(int categoryID) {
         String sql = "SELECT * FROM Category WHERE CategoryID = ?";
-        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+        try (PreparedStatement ps = new DBContext().getConnection().prepareStatement(sql)) {
             ps.setInt(1, categoryID);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
@@ -31,7 +31,7 @@ public class CategoryDAO extends DBContext {
     public List<Category> getAllCategories() {
         List<Category> categories = new ArrayList<>();
         String sql = "SELECT * FROM Category";
-        try (Statement stmt = connection.createStatement();
+        try (Statement stmt = new DBContext().getConnection().createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
             while (rs.next()) {
                 Category category = new Category();
